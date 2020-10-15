d96dc689b3c2996fbc5058ce51671fc0476798e6
Sakshat Shinde <sakshat-shinde@mitaoe.ac.in>
2020-10-15 14:30:32 +0530

    Update main.py
    broke the application

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from converter import convert_batch, convert_file
+from converter import c_file_DOCX_PDF, c_file_DOCX_PDF, c_file_PPT_PDF
 
-# Convert docx to PDF
-convert_file('./dummy_test_files/test.docx')
+# Convert docx to PDF -> Single file
+# c_file_DOCX_PDF('./dummy_test_files/test.docx')
+
+# convert PPT to PDF -> Single file
+c_file_PPT_PDF('./dummy_test_files/test2.ppt', './dummy_test_files')
